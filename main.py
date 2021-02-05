b632c6557b103b3c088f02742c8f675c92697122
TERRUSS <olivier.benaben@protonmail.com>
2021-02-05 13:59:33 +0100

    spaces -> tab

--- main.py
+++ main.py
@@ -18,11 +18,11 @@ LOW_REWARD = 0.1
 
 class Packet:
   def __init__(self, packetId, content):
-    self.packetId = packetId
-    self.content = content
+	self.packetId = packetId
+	self.content = content
 
   def __str__(self):
-    return "packet: "+str(self.packetId)+", Message: "+str(self.content)
+	return "packet: "+str(self.packetId)+", Message: "+str(self.content)
   
 
 
@@ -30,73 +30,73 @@ class Packet:
 class Trame:
   """A trame is a set of sloat"""
   def __init__(self):
-    self.slots = [[] for i in range(SLOTS_COUNT)]
+	self.slots = [[] for i in range(SLOTS_COUNT)]
 
   def addPacket(self, packet, nCopies):
-    """
-    Ajout a la trame un packet * nCopies
-    Add multiple copies of a packet in the slots of the trame
-    """
-    if (nCopies <= SLOTS_COUNT):
-      for i in range(0, nCopies):          
-        slotID = int(uniform(0, SLOTS_COUNT))
-        while (packet in self.slots[slotID]):
-          slotID = int(uniform(0, SLOTS_COUNT))  # Get randomly a slot index
-        self.slots[slotID].append(packet)
-    else:
-      print("Err Trame.addPacket : nCopies trop grand")
+	"""
+	Ajout a la trame un packet * nCopies
+	Add multiple copies of a packet in the slots of the trame
+	"""
+	if (nCopies <= SLOTS_COUNT):
+	  for i in range(0, nCopies):          
+		slotID = int(uniform(0, SLOTS_COUNT))
+		while (packet in self.slots[slotID]):
+		  slotID = int(uniform(0, SLOTS_COUNT))  # Get randomly a slot index
+		self.slots[slotID].append(packet)
+	else:
+	  print("Err Trame.addPacket : nCopies trop grand")
 
   def __str__(self):
-    s = ""
-    for i in range(SLOTS_COUNT):
-      s += str(i) + " : { "
-      for j in self.slots[i].packets:
-        s += f"{j} "
-      s += "}\n"
-    return s
+	s = ""
+	for i in range(SLOTS_COUNT):
+	  s += str(i) + " : { "
+	  for j in self.slots[i].packets:
+		s += f"{j} "
+	  s += "}\n"
+	return s
 
 
   def rewardIteration(self):
-    """
-    Calcule la reward pour l'iteration
-    """
+	"""
+	Calcule la reward pour l'iteration
+	"""
 
-    equipementRewards = defaultdict(float)
-    duplicatas = []
-    newSlots = list()
+	equipementRewards = defaultdict(float)
+	duplicatas = []
+	newSlots = list()
 
-    for i in range(0, SLOTS_COUNT):
-      newSlots.append(list())
+	for i in range(0, SLOTS_COUNT):
+	  newSlots.append(list())
 
-    # packet unique => HIGHT REWARD
-    for i in range(SLOTS_COUNT):
-      if len(self.slots[i]) == 1:
-        packet = self.slots[i][0]
-        if packet not in duplicatas:
-          equipementRewards[packet.packetId] = HIGH_REWARD
-          self.slots[i].remove(packet)
-          newSlots[i].append(packet)
+	# packet unique => HIGHT REWARD
+	for i in range(SLOTS_COUNT):
+	  if len(self.slots[i]) == 1:
+		packet = self.slots[i][0]
+		if packet not in duplicatas:
+		  equipementRewards[packet.packetId] = HIGH_REWARD
+		  self.slots[i].remove(packet)
+		  newSlots[i].append(packet)
 
-    i = 0
+	i = 0
 
-    # On cherche les packets a MEDIUM REWARD
-    while i != len(self.slots):
-      if len(self.slots[i]) == 1:
-        packet = self.slots[i][0]
-        equipementRewards[packet.packetId] = MEDIUM_REWARD
-        newSlots[i].append(packet)
+	# On cherche les packets a MEDIUM REWARD
+	while i != len(self.slots):
+	  if len(self.slots[i]) == 1:
+		packet = self.slots[i][0]
+		equipementRewards[packet.packetId] = MEDIUM_REWARD
+		newSlots[i].append(packet)
 
-        for j in range(len(self.slots)):
-          if packet in self.slots[j]:
-            self.slots[j].remove(packet)
-            # en supprimant un packet on peut creer des conflits
-            # => on recommence à 0
-            i = 0
-      i += 1
+		for j in range(len(self.slots)):
+		  if packet in self.slots[j]:
+			self.slots[j].remove(packet)
+			# en supprimant un packet on peut creer des conflits
+			# => on recommence à 0
+			i = 0
+	  i += 1
 
-    self.slots = newSlots
+	self.slots = newSlots
 
-    return equipementRewards
+	return equipementRewards
 
 
 ''' Fonctions '''
@@ -106,7 +106,7 @@ def get_poisson_distribution(equipment_count, lam):
   packets_count = dict()
 
   for e in range(equipment_count):
-    packets_count[e] = numpy.random.poisson(lam)
+	packets_count[e] = numpy.random.poisson(lam)
 
   return packets_count
 
@@ -119,30 +119,30 @@ def exec_strategie(n, nbEquipement, nbTrames, _lambda):
 
   #Pour chaque trame on envoi un packet et on recoit une recompense
   for i in range(nbTrames):
-    trame = Trame()
+	trame = Trame()
 
-    #Pour chaque equipement on envoie k copie des packets
-    for equip in range(nbEquipement):
-      if nbPackets[equip] != 0:
-        trame.addPacket(Packet(equip, equip), n)
-        nbPackets[equip] -= 1
-        packetEnvoyer[equip] = True
+	#Pour chaque equipement on envoie k copie des packets
+	for equip in range(nbEquipement):
+	  if nbPackets[equip] != 0:
+		trame.addPacket(Packet(equip, equip), n)
+		nbPackets[equip] -= 1
+		packetEnvoyer[equip] = True
 
-    recompence = trame.rewardIteration()
+	recompence = trame.rewardIteration()
 
-    # Ajoute LOW REWARD pour les equipements aillant pas reçu de recompence
-    for equip in range(nbEquipement):
-      if packetEnvoyer[equip] and equip not in recompence:
-        recompence[equip] = LOW_REWARD
+	# Ajoute LOW REWARD pour les equipements aillant pas reçu de recompence
+	for equip in range(nbEquipement):
+	  if packetEnvoyer[equip] and equip not in recompence:
+		recompence[equip] = LOW_REWARD
 
-    for reward in recompence.values():
-      recompences.append(reward)
+	for reward in recompence.values():
+	  recompences.append(reward)
 
   if (sum(recompences) == 0):
-      return 0
+	  return 0
   else:
-      return sum(recompences) / len(recompences)
-    
+	  return sum(recompences) / len(recompences)
+	
 
 def ucb1(nbEquipement, nbTrames, _lambda):
   #Renvoie le nombre de copie qu'un équipement envoie par trame qui MAXIMISE une formule
@@ -154,11 +154,11 @@ def ucb1(nbEquipement, nbTrames, _lambda):
 
   #Execute chaque strategie
   for k in cyclesVal:
-    recompences[k].append(exec_strategie(k, nbEquipement, nbTrames, _lambda))
+	recompences[k].append(exec_strategie(k, nbEquipement, nbTrames, _lambda))
   
   res = dict()
   for k in valeurs:
-    res[k] = sum(recompences[k]) / cycles + math.sqrt(2 * (math.log(cycles*3, math.e) / cycles))
+	res[k] = sum(recompences[k]) / cycles + math.sqrt(2 * (math.log(cycles*3, math.e) / cycles))
 
   return max(res, key=res.get)
 
